fcf05310d6610b5ff6cc727b07ab94b67fe88de4
Olga-allgood <olga.s.orlova@gmail.com>
2026-08-26 12:07:14 -0700

    Add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createRoot(
   document.getElementById("root")
